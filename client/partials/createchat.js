f8ff7437f6dc73ffa2686c67f292da15737a98de
James Rakratchatakul <james.rak@outlook.co.th>
2017-04-26 18:10:37 +0700

    add createchat

---
+++ client/partials/createchat.js
@@ -0,0 +1,13 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+import { Participants } from '../../imports/api/participants';
+
+import './chat.scss';
+import './chat.html';
+
+Template.createchat.onCreated(function chatOnCreated() {
+});
+
+Template.createchat.helpers({
+});
